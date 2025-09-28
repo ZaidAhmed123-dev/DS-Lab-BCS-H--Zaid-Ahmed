bf7b16e6833abf44ac9a8cabcec4bf467ed82063
ZaidAhmed123-dev <zaidahmed141@gmail.com>
2025-09-28 18:41:01 +0500

    Create task 3.cpp

---
+++ LAB-5/task 3.cpp
@@ -0,0 +1,19 @@
+#include <iostream>
+using namespace std;
+
+int sumTail(int n, int total=0) {
+    if(n==0) return total;
+    return sumTail(n-1, total+n);
+}
+
+int sumNonTail(int n) {
+    if(n==0) return 0;
+    return n + sumNonTail(n-1);
+}
+
+int main() {
+    int x=5;
+    cout << "Tail Recursion: " << sumTail(x) << endl;
+    cout << "Non-Tail Recursion: " << sumNonTail(x) << endl;
+    return 0;
+}
