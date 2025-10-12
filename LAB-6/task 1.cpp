574547ad4425911322345c6269e74061fa0000df
ZaidAhmed123-dev <zaidahmed141@gmail.com>
2025-10-12 21:16:08 +0500

    Create task 1.cpp

---
+++ LAB-6/task 1.cpp
@@ -0,0 +1,69 @@
+#include<iostream>
+using namespace std;
+
+class Stack{
+	
+	public:
+		int top;
+		int *arr;
+		int max;
+		Stack(int m):max(m){
+			arr=new int(max);
+			top=-1;
+		}	
+		
+		void push(int val)
+		{
+			if(top >= (max-1)){
+				cout<<"stack overflow";
+				return;
+			}
+			
+			arr[++top]=val;
+			cout<<"\nvalue "<<val<<" pushed into stack: "<<endl;
+		}
+		
+		void pop()
+		{
+			if(top < 0)
+			{
+				cout<<"\nstack underflow\n";
+				return;
+			}
+			
+			int x=arr[top--];
+			cout<<"\nvalue "<<x<<" popped out of stack"<<endl;
+		}
+		
+		void peek()
+		{
+			cout<<"\ntop value in stack is: "<<arr[top]<<endl;
+		}
+		
+		void display()
+		{
+			cout<<"\ndisplaying array: \n\n";
+			for(int i=0;i<top;i++)
+			{
+				cout<<arr[i]<<" ";
+			}
+		}
+		
+		
+};
+
+int main()
+{
+	Stack s(5);
+
+	s.push(5);
+
+	s.peek();
+	s.push(6);
+	s.push(7);
+	s.push(8);
+	s.peek();
+	s.pop();
+
+	s.display();
+}
